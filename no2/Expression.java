62f3db1765cf0ac4d3ee658b1a9d5968fa2b2529
wqf <1724840879@qq.com>
2021-10-25 13:43:07 +0800

    完全完成了带括号的算式算法

--- no2/Expression.java
+++ no2/Expression.java
@@ -1,7 +1,5 @@
 package no2;
 
-import java.util.ArrayList;
-
 public class Expression {
 	Element head = null;
 	Element tail = null;
@@ -13,8 +11,38 @@ public class Expression {
 		this.tail = tail;
 	}
 
-	// 无括号情况下expression获得result
 
+	public boolean getRealResult() {
+		
+		//如果算式中无括号，那么调用无括号获得result方法
+		
+		if(!this.haveParentheses()) {
+			if(getResult())return true;
+			return false;
+		}
+		
+		//如果是算式中有括号，那么削去一层括号，再递归调用自己算结果
+		Element front = head;
+		Element rear = front.nextElement;
+		while(!(front.flag<0)) {
+			front=front.nextElement;
+		}
+		rear=front.nextElement;
+		while(!(rear.flag>0)) {
+			rear=rear.nextElement;
+		}
+		
+		//以上一套下来后，front指向左括号，rear指向了右括号
+		front.flag+=1;
+		rear.flag-=1;
+		Expression exp=new Expression(front,rear);
+		if(!exp.getRealResult())return false;
+		if(!this.getRealResult())return false;
+		return true;
+	}
+	
+	// 无括号情况下expression获得result
+	
 	public boolean getResult() {
 		Element front = head;
 		Element rear = head.nextElement;
@@ -52,45 +80,7 @@ public class Expression {
 		return true;
 	}
 
-//	private void RemoveParentheses() {
-//
-//		
-//		while(true) {
-//			Element front =head;
-//			Element rear= head;
-//		while(front.flag==0) {
-//			front=front.nextElement;
-//		}
-//		rear=front.nextElement;
-//		while(rear.flag==0) {
-//			rear=rear.nextElement;
-//		}
-//		if(front.nextElement==rear) {
-//			front.flag++;
-//			rear.flag--;
-//			Element.Arithmetic(front,rear);
-//		}else {
-//			front=front.nextElement;
-//			Element.Arithmetic(front,rear);
-//		}
-//		if(!(front.nextElement==null)) {
-//			front=front.nextElement;
-//			
-//		}else if(rear.flag!=0) {
-//			rear=front;
-//			front=head;
-//			while(front.flag==0) {
-//				front=front.nextElement;
-//			}
-//			front.flag++;
-//			rear.flag--;
-//			Element.Arithmetic(front,rear);
-//			break;
-//		}else break;
-//		}
-//		
-//	}
-
+	@SuppressWarnings("null")
 	public boolean isAllValueSame(Expression exp) {
 
 		// 看对于本算式中的每一个元素是否都在exp中，如果有一个元素不在里面则return false
@@ -106,6 +96,7 @@ public class Expression {
 		return true;
 	}
 
+	@SuppressWarnings("null")
 	public boolean isAllOperatorSame(Expression exp) {
 
 		// 看对于本算式中的每一个元素是否都在exp中，如果有一个元素不在里面则return false
@@ -142,41 +133,56 @@ public class Expression {
 		return false;
 	}
 	
+	public boolean haveParentheses() {
+		int left=0;
+		int right=0;
+		Element p=head;
+		while(p!=tail.nextElement) {
+			if(p.flag<0)left+=Math.abs(p.flag);
+			else if(p.flag>0)right+=Math.abs(p.flag);
+			p=p.nextElement;
+		}
+		//只有左括号和右括号的个数都不为0，且个数相等时，才认为有括号
+		if(left>0&&right>0&&left==right)return true;
+		return false;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Element e1 = Element.ramdomElement("INT", 5);
-		Element e2 = Element.ramdomElement("TF", 5);
-		Element e3 = Element.ramdomElement("BF", 5);
-		Element e4 = Element.ramdomElement("INT", 5);
-		e1.nextElement = e2;
-		e2.nextElement = e3;
-		e3.nextElement = e4;
-		e1.randomOperator();
-		e2.randomOperator();
-		e3.randomOperator();
-		e1.printAll();
-		e2.printAll();
-		e3.printAll();
-		e4.print();
-		Expression expression = new Expression(e1, e4);
-		if (expression.getResult()) {
-			System.out.println("");
-			System.out.println("运算结果为：");
-			expression.result.print();
-		} else
-			System.out.println("该算式不符合规范");
-
-//		Element e1=new Element(1,0,1,'*');
-//		Element e2=new Element(0,1,2,'*');
-//		Element e3=new Element(4,4,5,'-');
-//		Element e4=new Element(3,0,1,' ');
-//		e1.nextElement=e2;
-//		e2.nextElement=e3;
-//		e3.nextElement=e4;
-//		Expression expression =new Expression(e1,e4);
-//		if(expression.getResult())
+//
+//		Element e1 = Element.ramdomElement("INT", 5);
+//		Element e2 = Element.ramdomElement("TF", 5);
+//		Element e3 = Element.ramdomElement("BF", 5);
+//		Element e4 = Element.ramdomElement("INT", 5);
+//		e1.nextElement = e2;
+//		e2.nextElement = e3;
+//		e3.nextElement = e4;
+//		e1.randomOperator();
+//		e2.randomOperator();
+//		e3.randomOperator();
+//		e1.printAll();
+//		e2.printAll();
+//		e3.printAll();
+//		e4.print();
+//		Expression expression = new Expression(e1, e4);
+//		if (expression.getResult()) {
+//			System.out.println("");
+//			System.out.println("运算结果为：");
 //			expression.result.print();
-//		else System.out.println("算式不符合要求");
+//		} else
+//			System.out.println("该算式不符合规范");
+
+		Element e1=new Element(3,0,2,'*',0);
+		Element e2=new Element(3,0,2,'+',-1);
+		Element e3=new Element(5,0,2,' ',1);
+		Element e4=new Element(0,0,1,' ',0);
+		e1.nextElement=e2;
+		e2.nextElement=e3;
+		e3.nextElement=e4;
+		Expression expression =new Expression(e1,e4);
+		if(expression.getRealResult())
+			expression.result.print();
+		else System.out.println("算式不符合要求");
+//		if(expression.haveParentheses())System.out.print("这个算式有括号");
+//		else System.out.print("这个算式无括号");
 	}
 }
--- no2/ExpressionTest.java
+++ no2/ExpressionTest.java
@@ -7,17 +7,8 @@ import org.junit.jupiter.api.Test;
 class ExpressionTest {
 
 	@Test
-	void testGetResultInNoParentheses() {
-////		Expression expression=new Expression();
-//		expression.head=new Element(5,0,0,'+');
-//		expression.head.nextElement=new Element(8,0,0,'+');
-//		expression.head.nextElement.nextElement=new Element(6,0,0,' ');
-//		expression.getResultInNoParentheses();
-//		Element e=new Element(19,0,0,' ');
-//		assertEquals(19,expression.head.wholeNumber);
-//		assertEquals(0,expression.head.numerator);
-//		assertEquals(0,expression.head.denominator);
-//		assertEquals(0,expression.head.operator);
+	void testGetRealResult() {
+		fail("Not yet implemented");
 	}
 
 }
